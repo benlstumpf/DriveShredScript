e6435800b6e3495dda1ba421365df9f393794877
Ben Stumpf <Benlstumpf@gmail.com>
2017-04-20 14:40:44 -0500

    Add Comments
    Changed filename and added comments

---
+++ DriveShredScript.sh
@@ -0,0 +1,8 @@
+
+lsblk -ld | grep -o '^sd[b-z]' | while read -r filename; do #find the drives needing to be shreded
+    filename="/dev/$filename" # Assign one drive path to a variable at a time
+    # Part 1: Run shred using variable
+    # Part 2: Get Serial Number of drive
+    # Part 3: Log output in file named using the Serial Number
+    shred -fvz "$filename" > "/home/ben/ProjectsInProgress/ShredLogs/$(hdparm -I "$filename" | grep -o 'Serial Number:\s*\S*' | cut -f2 -d: | sed 's/^\s*//').log" 2>&1 &
+done
